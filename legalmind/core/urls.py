8f4b29b82d91e41fa4368d3581393bf2ad8d877d
Rohith Vijayan <rohithvij20@gmail.com>
2025-01-12 20:00:01 +0530

    rag chatbot basic ui done

--- legalmind/core/urls.py
+++ legalmind/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path("",views.home,name='home')
+    path("",views.home,name='home'),
+    path("ragbot",views.ragbot,name='ragbot')
 ]
--- legalmind/core/views.py
+++ legalmind/core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
+
+def ragbot(request):
+    return render(request, 'core/ragbot.html')
